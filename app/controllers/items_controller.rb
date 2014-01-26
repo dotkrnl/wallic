4b45af0e5560931c9851bf580a18826aaf7e29e8
Jason Lau <i@dotkrnl.com>
2014-01-26 13:57:59 +0800

    fix redirect_to called multiple times

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,19 +4,21 @@ class ItemsController < ApplicationController
     @wallet = Wallet.find(params[:wallet_id])
     if not @wallet.write? session
       redirect_to @wallet, :alert => 'Permission denied'
+    else
+      @item = @wallet.items.create(item_params)
+      redirect_to @wallet
     end
-    @item = @wallet.items.create(item_params)
-    redirect_to @wallet
   end
 
   def destroy
     @wallet = Wallet.find(params[:wallet_id])
     if not @wallet.write? session
-      redirect_to @wallet, :alert => 'Permission denied'
+      redirect_toi :back, :alert => 'Permission denied'
+    else
+      @item = @wallet.items.find(params[:id])
+      @item.destroy
+      redirect_to :back 
     end
-    @item = @wallet.items.find(params[:id])
-    @item.destroy
-    redirect_to @wallet
   end
 
 private
