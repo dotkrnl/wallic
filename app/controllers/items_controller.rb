6eeae8eea9281ef62ddf4e89adc48c9e00a6d3b8
Jason Lau <i@dotkrnl.com>
2014-01-30 16:29:00 +0800

    make code more readable

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,11 @@
 class ItemsController < ApplicationController
 
   def create
-    @wallet = Wallet.find(params[:wallet_id])
+    @wallet = Wallet.find params[:wallet_id]
     if not @wallet.write? session
       redirect_to @wallet, :alert => 'Permission denied'
     else
-      @item = @wallet.items.new(item_params)
+      @item = @wallet.items.new item_params
       if not @item.save
         redirect_to @wallet, :alert => 'Bad information'
       else
@@ -15,22 +15,22 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @wallet = Wallet.find(params[:wallet_id])
+    @wallet = Wallet.find params[:wallet_id]
     if not @wallet.write? session
       redirect_to :back, :alert => 'Permission denied'
     else
-      @item = @wallet.items.find(params[:id])
+      @item = @wallet.items.find params[:id]
       @item.destroy
       redirect_to :back 
     end
   end
 
   def index
-    @wallet = Wallet.find(params[:wallet_id])
+    @wallet = Wallet.find params[:wallet_id]
     if not @wallet.read? session
       redirect_to :back, :alert => 'Permission denied'
     else
-      @items = @wallet.items.paginate(:page => params[:page])
+      @items = @wallet.items.paginate :page => params[:page]
     end
   end
 
